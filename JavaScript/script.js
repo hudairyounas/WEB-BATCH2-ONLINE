a083f6bb5d603e047ee6e1bcc031f44767253493
hudairyounas <hudairshorts@gmail.com>
2026-03-11 20:50:04 +0500

    js class 3

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,32 @@
-var a = 10454544545;
-var b = 20214242524;
-var c = 2012424545;
-console.log(a + b - c)
-//? my first name
-//? myFirstName
+// var a = 10454544545;
+// var b = 20214242524;
+// var c = 2012424545;
+// console.log(a + b - c)
+// //? my first name
+// //? myFirstName
+
+// var myFirstName = BigInt(422444454534153486454545454785475454545);
+// console.log(myFirstName)
+
+// char a = 'a';
+// var a= 10 + 20;
+// console.log(a);
+
+// //? + - * / % ** 
+// var a = 10;
+// var b = 20;
+// console.log(a % b) //? 2*2*2
+// var c = (12 + 2) * 2;
+
+// var a = "10";
+// var b = 10;
+
+// console.log(a == b) //? false
+// console.log(a === b) //? false
+
+
+var a = "10";
+var b = 20;
+// //? concatenation
+// console.log(a - b)
+console.log(a !== b)
