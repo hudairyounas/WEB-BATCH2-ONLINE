9c668a142f6741d5e79f14b2a1199e4aaa9d254f
hudairyounas <hudairshorts@gmail.com>
2026-03-12 20:35:54 +0500

    js class 4

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -25,8 +25,61 @@
 // console.log(a === b) //? false
 
 
-var a = "10";
-var b = 20;
+// var a = "10";
+// var b = 20;
 // //? concatenation
 // console.log(a - b)
-console.log(a !== b)
+// console.log(a !== b)
+
+
+// var num = BigInt(65445645645454545458786787857887);
+// var num2 = 65445645645454545458786787857887n;
+// console.log(num)
+
+// BigInt(4441545454545454545)
+
+//? increment and decrement
+
+// var a = 10;
+// var b = 20;
+// // a = 1 - a;
+// a = a--;
+// console.log(a)
+// console.log(a !== b)
+var a = 10;
+var b = 80;
+var c = 50;
+var d = 90;
+
+// console.log(a > 10 && b < a);
+console.log(a > 10 || b == a && c > d || !(d > c));
+
+// console.log(!true);
+
+// 5 4 1
+
+
+
+
+
+
+
+
+
+
+
+// post increment
+// a = a++;
+
+//? pre increment
+//? ++a
+
+
+
+//? a++
+//? a = a + 1
+
+//? a--
+//? a = a - 1
+// a = --a;
+// console.log(a)
