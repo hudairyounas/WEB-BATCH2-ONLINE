08d707b2068ce4158e73fa238f40ecc99d5c330f
hudairyounas <hudairshorts@gmail.com>
2026-04-20 19:39:50 +0500

    js class 18

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -665,11 +665,11 @@ const months = ["Jan", "march", "April", "June", "July"];
 
 //? match() : Returns an array of the matched values or null if no match is found.
 
-let text = "Hello JavaScript, Jelcome Futureplix JavaScript course";
-let result = text.match(/JavaScript/g);
+// let text = "Hello JavaScript, Jelcome Futureplix JavaScript course";
+// let result = text.match(/JavaScript/g);
 // * regex = /JavaScript/g
 // // let result = text.match("JavaScript");
-console.log(result);
+// console.log(result);
 
 //? matchAll() : Returns an iterator of all matches, providing detailed information about each match. Returns an empty iterator if no match is found.
 // let text = "I have 2 cats and 3 dogs dogs";
@@ -717,7 +717,7 @@ console.log(result);
 
 // let result = text.slice(6);
 // let result = text.slice(6, 15);
-console.log(result);
+// console.log(result);
 
 //! Replacing String Content:
 //? replace() : The replace method is used to replace a specified value with another value.
@@ -728,7 +728,7 @@ console.log(result);
 
 //! Other Useful Methods:
 //? toUpperCase and toLowerCase: Converts the string to uppercase or lowercase.
-// const str = "JavaScript";
+// const str = "JAVASCRIPT";
 // console.log(str.toUpperCase()); // Outputs: JAVASCRIPT
 // console.log(str.toLowerCase()); // Outputs: javascript
 
@@ -736,11 +736,104 @@ console.log(result);
 // const str = "   Hello, World!   ";
 // console.log(str.length);
 
-// let trimStr = str.trim();
+// let trimStr = str.trimEnd();
 // console.log(trimStr);
 // console.log(trimStr.length);
 
 //? split: Splits the string into an array of substrings based on a specified delimiter.
-// const str = "apple,orange,banana";
-// let strArr = str.split(",").reverse().join();
-// console.log(strArr);
+// const str = "Hello, World!";
+// console.log(str.split(""))
+
+
+// const str = "Hello my name is hudair";
+// let strArr = str.split(" ");
+// let reverse = strArr.reverse()
+// let join = reverse.join(" ")
+// console.log(join);
+
+//* =========================================
+//* Math Object in JavaScript
+//* =========================================
+
+//* Math: The Math namespace object contains static properties and methods for mathematical constants and functions.
+//* Math works with the Number type. It doesn't work with BigInt.
+
+//? 1: Constants:
+//* - Math.PI: Represents the mathematical constant Pi (π).
+// const piValue = Math.PI;
+// console.log(piValue);
+
+//* =========================================
+//* 2. Basic Operations:
+//* =========================================
+
+//? Math.abs(): The Math.abs() static method returns the absolute value of a number.
+// or in simple, how far the number is from 0. It will be always positive
+
+// console.log(Math.abs(5));
+// console.log(Math.abs(-115));
+
+//? Math.round(x): Rounds a number to the nearest integer.
+// const roundedValue = Math.round(3.4);
+// console.log(roundedValue);
+
+//? Math.ceil(x): Returns the value of x rounded up to its nearest integer:
+// const ceilValue = Math.ceil(3.3);
+// console.log(ceilValue);
+
+//? Math.floor(x): Returns the value of x rounded down to its nearest integer.
+// const floorValue = Math.floor(3.7);
+// console.log(floorValue);
+
+//? Math.trunc(x): Returns the integer part of x:
+// const truncValue = Math.trunc(3.7);
+// console.log(truncValue);
+
+//todo Math.trunc() and Math.sign() were added to JavaScript 2015 - ES6.
+// const truncValue = Math.trunc(-3.7);
+// const floorValue = Math.floor(-3.4);
+// console.log(truncValue);
+// console.log(floorValue);
+
+//todo Notes:
+// No matter how many chars are there after decimal, they all will always return only number before the decimal.
+// round rounds to the nearest integer.
+// floor always rounds down.
+// ceil always rounds up.
+
+//* =========================================
+//* 3. Exponential and Logarithmic Functions:
+//* =========================================
+
+//? Math.pow(x, y): Returns the value of x to the power of y.
+// console.log(Math.pow(2, 5));
+// console.log(2 ** 5);
+
+//? Math.sqrt(): Math.sqrt(x) returns the square root of x:
+// let squareRoot = Math.sqrt(25);
+// console.log(squareRoot);
+
+//? Math.log(x) returns the natural logarithm of x.
+// let logResult = Math.log(1);
+// let logResult = Math.log(2);
+// console.log(logResult);
+
+//? Math.log2(x) returns the base 2 logarithm of x.
+// let logResult = Math.log2(8);
+// console.log(logResult);
+
+//* =========================================
+//* Interview Question
+//* =========================================
+
+//! Generate Random number
+//? Math.random(): Math.random() returns a random number between 0 (inclusive), and 1 (exclusive)
+// console.log(Math.floor(Math.random() * 1000))
+
+// console.log((Math.random() * 100).toFixed());
+
+
+// console.log(Math.trunc(4.9));   // 4
+// console.log(Math.trunc(4.1));   // 4
+// console.log(Math.trunc(-4.9));  // -4
+// console.log(parseInt(4.9))   // 4 (but not recommended for math)
