b76bc086b8320686f22cb1e2a85496b74d67a76a
hudairyounas <hudairshorts@gmail.com>
2026-04-29 20:38:04 +0500

    js class 22

---
+++ JavaScript/date_time/script.js
@@ -0,0 +1,17 @@
+let date = new Date(1777472758355)
+// let mili = date.getTime()
+
+console.log(date)
+//? E = mc2
+
+// 1hour = 24hour
+// 2019
+// 21 => 1 year
+// 70 80
+
+// sun => 8min
+// moon 1.6
+// KB 
+// intelligent 
+
+// painting
---
+++ JavaScript/localStorage/script.js
@@ -0,0 +1,13 @@
+// let arr = [1, 2, 3, 4]
+
+
+// //? JSON.stringify()
+// //? JSON.parse()
+
+// // localStorage.setItem("name", JSON.stringify(arr))
+// console.log(JSON.parse(localStorage.getItem("name")))
+
+// localStorage.getItem("name")
+// sessionStorage.getItem
+
+// // localStorage.removeItem("name")
