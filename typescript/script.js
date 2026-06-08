4b78a868cea37dfc03e1c6ba5b497c9a7b897741
hudairyounas <hudairshorts@gmail.com>
2026-06-08 20:34:56 +0500

    typescript class 3

--- typescript/script.js
+++ typescript/script.js
@@ -1,8 +1,8 @@
 "use strict";
-//? 1- npm install -g typescript 
-//? 2 - tsc --init  => one time 
-//? 3 - tsc -w => every time 
-//? 4 - node script.js => every time 
+//? 1- npm install -g typescript
+//? 2 - tsc --init  => one time
+//? 3 - tsc -w => every time
+//? 4 - node script.js => every time
 Object.defineProperty(exports, "__esModule", { value: true });
 // let username: string = "str";
 // let id: number = 2;
@@ -20,6 +20,66 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // console.log(sum,sum2);
 // let arr: string[] = ["a", "b", "c"];
 // console.log(arr)
-let arr = [1, "a"];
-console.log(arr);
+// let arr:[number, string] = [1, "a"];
+// console.log(arr)
+// functions
+// function add(num1:number[], num2:number[]):number[] {
+//  return [...num1, ...num2];
+// }
+// console.log(add([10], [20]));
+// loops
+// for (let i: number = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+// objects
+// let obj: {
+//     name: string,
+//     id: number,
+//     bl: boolean,
+//     age: number
+// } = {
+//     name: "str",
+//     id: 2,
+//     bl:true,
+//     age: 23
+// }
+// type User = {
+//     name: string,
+//     id: number,
+//     bl: boolean,
+//     age: number,
+// }
+// const obj: User = {
+//     name: "ali",
+//     id: 1,
+//     bl:true,
+//     age: 23
+// }
+// const obj2: User = {
+//     name: "saad",
+//     id: 2,
+//     bl:false,
+//     age: 34
+// }
+// console.log(obj)
+// type Age = Number;
+// const num: Age = "212";
+// console.log(num);
+// type User = {
+//     name:string;
+//     id:number;
+//     age:number;
+//     bl:boolean;
+// }
+// type User2 = {
+//     address:string;
+// }
+// const Obj: User | User2 = {
+//     name: "ali",
+//     id: 1,
+//     bl:true,
+//     age: 23,
+//     address: "karachi"
+// }
+// console.log(Obj);
 //# sourceMappingURL=script.js.map
--- typescript/script.ts
+++ typescript/script.ts
@@ -1,7 +1,7 @@
-//? 1- npm install -g typescript 
-//? 2 - tsc --init  => one time 
-//? 3 - tsc -w => every time 
-//? 4 - node script.js => every time 
+//? 1- npm install -g typescript
+//? 2 - tsc --init  => one time
+//? 3 - tsc -w => every time
+//? 4 - node script.js => every time
 
 // let username: string = "str";
 // let id: number = 2;
@@ -10,15 +10,12 @@
 // let nu:null = null;
 // let an:any = 42;
 
-
 // console.log(username);
-
 
 // let num1: number = 10;
 // let num2: number = 20;
 // let num3: number = 2121;
 // let num4: string = "212";
-
 
 // let sum: number = num1 + num2 + num3;
 // let sum2: string = num1 + num2 + num4;
@@ -28,5 +25,84 @@
 // let arr: string[] = ["a", "b", "c"];
 // console.log(arr)
 
-let arr:[number, string] = [1, "a"];
-console.log(arr)
+// let arr:[number, string] = [1, "a"];
+// console.log(arr)
+
+// functions
+
+// function add(num1:number[], num2:number[]):number[] {
+//  return [...num1, ...num2];
+// }
+
+// console.log(add([10], [20]));
+
+// loops
+
+// for (let i: number = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+
+// objects
+
+// let obj: {
+//     name: string,
+//     id: number,
+//     bl: boolean,
+//     age: number
+// } = {
+//     name: "str",
+//     id: 2,
+//     bl:true,
+//     age: 23
+// }
+
+
+// type User = {
+//     name: string,
+//     id: number,
+//     bl: boolean,
+//     age: number,
+// }
+
+// const obj: User = {
+//     name: "ali",
+//     id: 1,
+//     bl:true,
+//     age: 23
+// }
+// const obj2: User = {
+//     name: "saad",
+//     id: 2,
+//     bl:false,
+//     age: 34
+// }
+
+// console.log(obj)
+
+// type Age = Number;
+
+// const num: Age = "212";
+
+// console.log(num);
+
+// type User = {
+//     name:string;
+//     id:number;
+//     age:number;
+//     bl:boolean;
+// }
+
+// type User2 = {
+//     address:string;
+// }
+
+// const Obj: User | User2 = {
+//     name: "ali",
+//     id: 1,
+//     bl:true,
+//     age: 23,
+//     address: "karachi"
+// }
+
+// console.log(Obj);
