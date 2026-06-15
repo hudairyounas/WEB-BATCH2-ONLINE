72e287bb5bb39fe0b55efbaba8dca21245700784
hudairyounas <hudairshorts@gmail.com>
2026-06-15 21:04:01 +0500

    react class 4

---
+++ import_export/main.js
@@ -0,0 +1,13 @@
+export function print() {
+    console.log("hello");
+}
+
+// export function print2() {
+//     console.log("hello2");
+// }
+
+// export function print3() {
+//     console.log("hello3");
+// }
+
+// export default print;
---
+++ import_export/script.js
@@ -0,0 +1,7 @@
+// import hudair from "./main.js";
+import { print as hudair } from "./main.js";
+
+hudair()
+// hudair();
+// print2();
+// print3();
