3c890d6007ab74103fa0ec4f9e68a6a8873b9421
hudairyounas <hudairshorts@gmail.com>
2026-06-16 20:53:38 +0500

    react class 5

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import Header from './components/Header'
+import "./App.css"
 
 const App = () => {
   return (
     <div>
+      <h1 className=''>Hello i am a H1 Heading</h1>
+      <Header />
+      <Header />
+      <Header />
+      <Header />
       <Header />
     </div>
   )
 }
 
 export default App
-// git init 
--- vite/src/components/Header.jsx
+++ vite/src/components/Header.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import "./Header.css"
 
 const Header = () => {
+
+  const class1 = "11th"
+  const class2 = "12th"
+
   return (
-    <div>Header</div>
+    <header>
+      {/* <p className="h1" style={{
+        color: "black",
+        backgroundColor: "lightgreen",
+        textAlign: "center"
+      }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, repudiandae facere veritatis a, suscipit aliquam placeat optio illum quia ad sit accusamus adipisci maxime nam provident, in molestias inventore similique.</p> */}
+      <ul>
+        <li>Ali reads in class {class1}</li>
+        <li>Zain reads in class {class2}</li>
+      </ul>
+    </header>
   )
 }
 
