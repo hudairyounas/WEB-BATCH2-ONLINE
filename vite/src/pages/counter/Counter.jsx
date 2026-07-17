a8528d2add30d4f22acb75d126da470c28894e3a
hudairyounas <hudairshorts@gmail.com>
2026-07-17 21:10:27 +0500

    react class 28

--- vite/src/pages/counter/Counter.jsx
+++ vite/src/pages/counter/Counter.jsx
@@ -1,32 +1,59 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { changeAge, changePassword, changeUsername } from '../../store/slices/user.slice'
+import { changeAge, changePassword, changeUsername, getUser } from '../../store/slices/user.slice'
 
 const Counter = () => {
 
-  const dispatch = useDispatch()
-  const userName = useSelector((store)=>store.userSlice.user1.userName)
-  const userAge = useSelector((store)=>store.userSlice.user1.age)
-  const userPassword = useSelector((store)=>store.userSlice.user1.password)
+  // const dispatch = useDispatch()
+  // const userName = useSelector((store)=>store.userSlice.user1.userName)
+  // const userAge = useSelector((store)=>store.userSlice.user1.age)
+  // const userPassword = useSelector((store)=>store.userSlice.user1.password)
 
-  const userNameChangeHandler = (e)=>{
-    dispatch(changeUsername(e.target.value))
-  }
-  const userAgeChangeHandler = (e)=>{
-    dispatch(changeAge(e.target.value))
-  }
-  const userPasswordChangeHandler = (e)=>{
-    dispatch(changePassword(e.target.value))
-  }
+  // const userNameChangeHandler = (e)=>{
+  //   dispatch(changeUsername(e.target.value))
+  // }
+  // const userAgeChangeHandler = (e)=>{
+  //   dispatch(changeAge(e.target.value))
+  // }
+  // const userPasswordChangeHandler = (e)=>{
+  //   dispatch(changePassword(e.target.value))
+  // }
+
+  const dispatch = useDispatch();
+
+  const userList = useSelector((store) => store.userSlice.userList);
+  const error = useSelector((store) => store.userSlice.error);
+  const isLoading = useSelector((store) => store.userSlice.isLoading);
+
+console.log(error)
+console.log(isLoading)
+  
+  console.log(userList);
+
+  useEffect(()=>{
+    dispatch(getUser());
+  }, [dispatch])
+
+  
 
   return (
     <div>
-      <h1>My name is {userName}</h1>
+      {
+        isLoading ? <h1>Data is Loading</h1> :
+        userList.map((user) =>{
+          return (
+            <h1 key={user.id}>{user.name}</h1>
+          )
+        })
+      }
+
+
+      {/* <h1>My name is {userName}</h1>
       <h1>My age is {userAge}</h1>
       <h1>My password is {userPassword}</h1>
       <input type="text" onChange={userNameChangeHandler} placeholder='enter your name' />
       <input type="text" onChange={userAgeChangeHandler} placeholder='enter your age'/>
-      <input type="text" onChange={userPasswordChangeHandler} placeholder='enter your password'/>
+      <input type="text" onChange={userPasswordChangeHandler} placeholder='enter your password'/> */}
     </div>
   )
 }
--- vite/src/store/slices/user.slice.js
+++ vite/src/store/slices/user.slice.js
@@ -1,4 +1,17 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+
+
+const getUser = createAsyncThunk("user/getUser", async () => {
+  try {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+    return res.data;
+  } catch (error) {
+    console.log(error);
+  }
+})
+
 
 export const userSlice = createSlice({
   name: "user",
@@ -8,20 +21,35 @@ export const userSlice = createSlice({
       age: "",
       password: "",
     },
+    userList:[],
+    error: null,
+    isLoading: true,
   },
   reducers: {
-    changeUsername: (state, action)=>{
-      console.log(action.payload)
-      state.user1.userName = action.payload
+    changeUsername: (state, action) => {
+      console.log(action.payload);
+      state.user1.userName = action.payload;
     },
     changeAge: (state, action) => {
-      state.user1.age = action.payload
+      state.user1.age = action.payload;
     },
-    changePassword: (state, action)=>{
-      state.user1.password = action.payload
-    }
+    changePassword: (state, action) => {
+      state.user1.password = action.payload;
+    },
+  },
+  extraReducers: (builder) =>{
+    builder.addCase(getUser.fulfilled, (state, action) => {
+      state.userList = action.payload;
+    })
+    .addCase(getUser.pending, (state, action) =>{
+      state.isLoading = false;
+    })
+    .addCase(getUser.rejected, (state, action) =>{
+      state.error = action.payload;
+    })
   }
 });
 
-export const {changeUsername, changeAge, changePassword} = userSlice.actions;
+export const { changeUsername, changeAge, changePassword } = userSlice.actions;
+export {getUser};
 export default userSlice.reducer;
