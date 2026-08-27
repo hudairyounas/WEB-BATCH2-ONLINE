34e80b41a47ebc1e66b8ab601abeea98b39da931
hudairyounas <hudairshorts@gmail.com>
2026-08-27 21:07:55 +0500

    backend class 5

--- node_js/server.js
+++ node_js/server.js
@@ -1,43 +1,39 @@
 import express from "express";
+import cors from "cors";
 
 //? intializing server
 const app = express();
+app.use(express.json());
+app.use(cors());
 
- 
-//? setting up api
-// app.get("/products", (req, res) => {
-//    const username = req.query.username;
-//    const email = req.query.email;
-//    const password = req.query.password;
-//   if (!username || !email || !password) {
-//     return res.status(400).json({message: "please provide a username, email, and password"})
-//   }
+app.post("/register", (req, res) => {
+  const { name, email } = req.body;
+  if (!name || !email) {
+    return res.status(400).json({ message: "please provide a name and email" });
+  }
+  console.log(name, email);
 
+  return res
+    .status(200)
+    .json({ message: "user created successfully", user: { name, email } });
+});
 
-//   return res.status(200).json({message: `products username: ${username}, email: ${email}, password: ${password}`})
-// })
+app.post("/api/user", (req, res) => {
+  const { name, email } = req.headers;
 
+  if (!name || !email) {
+    return res.status(400).json({ message: "please provide a name and email" });
+  }
 
-app.get("/products/:abc/:xyz/:prq",(req,res) => {
-    const {abc, xyz, prq} = req.params;
+  console.log(req.headers);
 
-    const {id} = req.query;
+  return res
+    .status(200)
+    .json({ message: "user created successfully", user: { name, email } });
+});
 
-    if (!abc || !xyz || !prq || !id){
-        return res.status(400).json({message: "please provide an params"})
-    }
-
-    return res.status(200).json({message: `product id: ${abc}, ${xyz}, ${prq}, ${id}`})
-    
-})
-
-//? http://localhost:5000/products/bhjbhbhjbh/xyz/hjhghv?id=21215154
-
-//? starting server
 app.listen(5000, () => {
-    console.log("server started on port 5000");
-})
-
-// axios.get("http://localhost:5000/")
+  console.log("server started on port 5000");
+});
 
 //? http://localhost:5000/products
