351bbffdaa8a7bcd6fbd6393b1970c863fc6fae2
hudairyounas <hudairshorts@gmail.com>
2026-09-17 22:35:04 +0500

    backend class 3

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,41 +1,94 @@
 import express from "express";
 
 const app = express();
-const PORT = 5000
+const PORT = 5000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.json({message: "Hello World get"})
-})
+  res.json({ message: "Hello World get" });
+});
 
 app.post("/", (req, res) => {
-  const {name, age, password} = req.query;
-
-
+  const { name, age, password } = req.query;
 
   if (!name || !age || !password) {
-    return res.status(400).json({success: false, message: "Please enter your name and age and password"})
+    return res.status(400).json({
+      success: false,
+      message: "Please enter your name and age and password",
+    });
   }
 
-  res.status(200).json({success: true, message: `Hello ${name} and your age is ${age} and password is ${password}`})
-})
-
+  res.status(200).json({
+    success: true,
+    message: `Hello ${name} and your age is ${age} and password is ${password}`,
+  });
+});
 
 app.post("/product/:abc/xyz", (req, res) => {
-
-  const {abc, xyz} = req.params;
-  const {city} = req.query;
-
+  const { abc, xyz } = req.params;
+  const { city } = req.query;
 
   if (!abc) {
-    return res.status(400).json({success: false, message: "Please enter your abc and xyz"})
+    return res
+      .status(400)
+      .json({ success: false, message: "Please enter your abc and xyz" });
   }
 
   if (!city) {
-    return res.status(400).json({success: false, message: "Please enter your city"})
+    return res
+      .status(400)
+      .json({ success: false, message: "Please enter your city" });
   }
 
-  res.status(200).json({success: true, message: `Hello abc is ${abc} and xyz is ${xyz} and your city is ${city}`})
-})
+  res.status(200).json({
+    success: true,
+    message: `Hello abc is ${abc} and xyz is ${xyz} and your city is ${city}`,
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.post("/product/create", (req, res) => {
+  console.log("product fetch.....", req.body);
+  const { name, email, password } = req.body;
+
+  if (!email || !name || !password) {
+    return res
+      .status(500)
+      .json({ success: false, message: "Please enter your email and name and password" });
+  }
+
+  return res
+    .status(200)
+    .json({ success: true, message: "user data fetch successfully", user: req.body });
+});
+//? ddos
+//? 1min 10000
+//? bot
+
+
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`server is running at port number ${PORT}`);
@@ -54,4 +107,8 @@ app.listen(PORT, () => {
 //? cookies
 //? files
 
-//? http://localhost:5000/product/bjhhhjb/xyz
+//? http://localhost:5000/user?name=john&age=25&password=12345
+
+//? https://github.com/openai
+//? https://github.com/anthropics
+//? https://github.com/hudairyounas?tab=repositories
