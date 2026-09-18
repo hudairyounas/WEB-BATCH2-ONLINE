271a4847e8c625859502002e7a8b02bb92f2380a
hudairyounas <hudairshorts@gmail.com>
2026-09-18 20:44:13 +0500

    backend class 4

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import multer from "multer";
 
 const app = express();
 const PORT = 5000;
 app.use(express.json());
+
+app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World get" });
@@ -46,49 +49,60 @@ app.post("/product/:abc/xyz", (req, res) => {
   });
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.post("/product/create", (req, res) => {
   console.log("product fetch.....", req.body);
   const { name, email, password } = req.body;
 
   if (!email || !name || !password) {
-    return res
-      .status(500)
-      .json({ success: false, message: "Please enter your email and name and password" });
+    return res.status(500).json({
+      success: false,
+      message: "Please enter your email and name and password",
+    });
   }
 
-  return res
-    .status(200)
-    .json({ success: true, message: "user data fetch successfully", user: req.body });
+  return res.status(200).json({
+    success: true,
+    message: "user data fetch successfully",
+    user: req.body,
+  });
 });
 //? ddos
 //? 1min 10000
 //? bot
 
+// app.post("/login",(req,res)=>{
+//   const name = req.headers;
+//   console.log(req.headers)
 
+//   if (!name) {
+//     res.status(400).json({message: "You are not authorized"})
+//   }
 
+//   return res.status(200).json({message: "You are authorized", user:name})
+// })
 
+//? multer
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
 
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + "." + file.mimetype.split("/")[1],
+    );
+  },
+});
 
+const upload = multer({ storage: storage });
 
-
-
+app.post("/upload", upload.single("image"), (req, res) => {
+  console.log(req.file);
+  res.json({ data: req.file });
+});
 
 app.listen(PORT, () => {
   console.log(`server is running at port number ${PORT}`);
